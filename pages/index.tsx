447372eb329fa27f8b6da6a2243c575968cd6164
Manish <manishsdlp222@gmail.com>
2023-03-25 16:40:48 +0530

    fix: Sidebar height

--- pages/index.tsx
+++ pages/index.tsx
@@ -66,7 +66,7 @@ export default function Home() {
                     </li>
                     <li
                       className={
-                        router.pathname == "/blog"
+                        router.pathname == "/"
                           ? "text-slate-900"
                           : "text-[#8F8E90]"
                       }
@@ -122,7 +122,7 @@ export default function Home() {
             {showMobileDropDown && (
               <div
                 id="sidenav"
-                className="fixed top-0 left-0 z-[1035] h-screen w-60 transition-transform -translate-x-full overflow-hidden bg-white shadow-[0_4px_12px_0_rgba(0,0,0,0.07),_0_2px_4px_rgba(0,0,0,0.05)] data-[te-sidenav-hidden='false']:-translate-x-0"
+                className="fixed top-0 left-0 z-[1035] h-full w-60 transition-transform -translate-x-full overflow-hidden bg-white shadow-[0_4px_12px_0_rgba(0,0,0,0.07),_0_2px_4px_rgba(0,0,0,0.05)] data-[te-sidenav-hidden='false']:-translate-x-0"
                 data-te-sidenav-init
                 data-te-sidenav-hidden="false"
                 data-te-sidenav-right="true"
